7be53d51859254d1f98dbab19e45a94f91409267
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-10 20:49:42 -0700

    categories styles and images

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
+import './categories.styles.scss';
+
 const App = () => {
 	const categories = [
 		{
 			id: 1,
-			title: 'Hats',
+			title: 'hats',
+			imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
 		},
 		{
 			id: 2,
-			title: 'Jackets',
+			title: 'jackets',
+			imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
 		},
 		{
 			id: 3,
-			title: 'Sneakers',
+			title: 'sneakers',
+			imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
 		},
 		{
 			id: 4,
-			title: 'Womens',
+			title: 'womens',
+			imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
 		},
 		{
 			id: 5,
-			title: 'Mens',
+			title: 'mens',
+			imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
 		},
 	];
 	return (
 		<div className="categories-container">
-			{categories.map((category) => {
+			{categories.map(({ title, id, imageUrl }) => {
 				return (
-					<div className="category-container" key={category.id}>
-						<div className="background-image"></div>
+					<div className="category-container" key={id}>
+						<div
+							className="background-image"
+							style={{ backgroundImage: `url(${imageUrl})` }}
+						></div>
 						<div className="category-body-container">
-							<h2>{category.title}</h2>
+							<h2>{title}</h2>
 							<p> Shop Now</p>
 						</div>
 					</div>
